e5a1520b7cc23d3b942f81be20524c365d67a5ef
Wes-White <wesleyrwhite86@gmail.com>
2020-01-15 18:31:03 -1000

    Created new branch, added state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React from "react";
+
+const defaultTasks = [
+  //using this to check if app is working with generic data
+  {
+    todo: "Learn Class components",
+    id: 1,
+    completed: false
+  },
+  {
+    todo: "Use Class components",
+    id: 2,
+    completed: false
+  },
+  {
+    todo: "Miss Hooks :(",
+    id: 3,
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: defaultTasks,
+      newTask: ""
+    };
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
